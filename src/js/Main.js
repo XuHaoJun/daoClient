35c3a049cb842e08c7ff85dcf1b2d27417d16e0c
XuHaoJun <XuHaoJunTW@gmail.com>
2014-12-08 14:40:53 +0800

    Tidy Main.js.

--- src/js/Main.js
+++ src/js/Main.js
@@ -5,18 +5,4 @@ var World = require('./World.js');
 window.onload = function() {
   var dao = new World();
   dao.run();
-  window.dao = dao;
 };
-
-// dao.once('connOnopen', function() {
-//   this.loginAccount("wiwi", "wiwi");
-// }.bind(dao));
-// dao.once('handleSuccessLoginAcccount', function() {
-//   this.account.loginChar(0);
-//   this.account.once('handleSuccessLoginChar', function() {
-//     window.c = this.account.usingChar;
-//   }.bind(this));
-// }.bind(dao));
-
-// var cp = require('chipmunk');
-// window.cp = cp;
